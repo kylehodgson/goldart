53d193be23adc7fd34f6e2692d2f07a86523816d
kylehodgson <kyle.hodgson@gmail.com>
2024-07-27 18:26:07 -0400

    bah, vim tabs.

--- gol.dart
+++ gol.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'dart:io';
 
 void main() {
-	var board = Board(15,15);
+  var board = Board(15, 15);
   while (true) {
     board.nextGeneration();
     print(board.toString());
@@ -12,7 +12,7 @@ void main() {
 }
 
 class Cell {
-	bool alive = false;
+  bool alive = false;
 
   Cell(this.alive);
 
@@ -23,28 +23,28 @@ class Cell {
 }
 
 extension RangeExtension on int {
-	List<int> upTo(int maxInclusive) =>
-		[for (int i = this; i <= maxInclusive; i++) i];
+  List<int> upTo(int maxInclusive) =>
+      [for (int i = this; i <= maxInclusive; i++) i];
 }
 
 class Board {
-	int rowSize = 0;
-	int colSize = 0;
-	var rows = [];
+  int rowSize = 0;
+  int colSize = 0;
+  var rows = [];
 
-	Board(this.rowSize, this.colSize);
+  Board(this.rowSize, this.colSize);
 
-	@override
-	String toString() {
-		String board = "";
-    this.rows.forEach( (column) {
-        column.forEach( (cell) {
-          board += '${cell}';
-        });
-        board += '\n';
+  @override
+  String toString() {
+    String board = "";
+    this.rows.forEach((column) {
+      column.forEach((cell) {
+        board += '${cell}';
+      });
+      board += '\n';
     });
     return board;
-	}
+  }
 
   bool willLive(int row, int col) {
     var neighbors = getLiveNeighborsFor(row, col);
@@ -56,15 +56,18 @@ class Board {
 
   int getLiveNeighborsFor(int myRow, int myCol) {
     int live = 0;
-    (myRow-1).upTo(myRow+1).forEach( (row)  {
-      (myCol-1).upTo(myCol+1).forEach( (col) {
-        if (myRow==row && myCol == col) {
+    (myRow - 1).upTo(myRow + 1).forEach((row) {
+      (myCol - 1).upTo(myCol + 1).forEach((col) {
+        if (myRow == row && myCol == col) {
           // don't count myself in neighbor count!
-        } else if(row >= this.rowSize || col >= this.colSize || row <= 0 || col <= 0) {
+        } else if (row >= this.rowSize ||
+            col >= this.colSize ||
+            row <= 0 ||
+            col <= 0) {
           // edge detected!
         } else {
           Cell neighbor = this.rows[row][col];
-          if(neighbor.alive) live++;
+          if (neighbor.alive) live++;
         }
       });
     });
@@ -79,19 +82,17 @@ class Board {
     return neighbors < 2;
   }
 
-	void nextGeneration() {
-		final rows = [];
-		0.upTo(this.rowSize-1).forEach( (row) {
+  void nextGeneration() {
+    final rows = [];
+    0.upTo(this.rowSize - 1).forEach((row) {
       final cols = [];
-			0.upTo(this.colSize-1).forEach( (col) { 
-        cols.add(
-          this.rows.length==0 ? 
-            Cell(Random().nextBool()) :
-            Cell(willLive(row, col))
-        );
-			});
+      0.upTo(this.colSize - 1).forEach((col) {
+        cols.add(this.rows.length == 0
+            ? Cell(Random().nextBool())
+            : Cell(willLive(row, col)));
+      });
       rows.add(cols);
-		});
+    });
     this.rows = rows;
-	}
+  }
 }
